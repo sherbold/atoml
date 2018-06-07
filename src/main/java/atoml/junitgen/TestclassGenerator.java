2c8e731b3f0936e626ceb79ca794a86032693c78
sherbold <herbold@cs.uni-goettingen.de>
2018-06-07 15:49:03 +0200

    Redesigned test class generator to now work with template files
    This replaces the really long inline strings and makes changes to the
    tests a lot easier

--- src/main/java/atoml/junitgen/TestclassGenerator.java
+++ src/main/java/atoml/junitgen/TestclassGenerator.java
@@ -1,6 +1,7 @@
 package atoml.junitgen;
 
 import java.util.List;
+import java.util.Scanner;
 
 import atoml.classifiers.ClassifierCreator;
 import atoml.metamorphic.MetamorphicOrderedDataTest;
@@ -52,29 +53,23 @@ public class TestclassGenerator {
 	 * @return the source code
 	 */
 	public String generateTestclass() {
-		StringBuilder testcaseString = new StringBuilder();
-		
-		testcaseString.append(packageName() + "\n");
-		testcaseString.append(junitImports() + "\n");
-		testcaseString.append(atomlImports() + "\n");
-		
-		testcaseString.append(classHead());
+		@SuppressWarnings("resource")
+		String classBody = new Scanner(this.getClass().getResourceAsStream("/junit-class.template"), "UTF-8").useDelimiter("\\A").next();
+
+		StringBuilder testmethods = new StringBuilder();
 		
 		for( MetamorphicTest metamorphicTest : metamorphicTests) {
-			testcaseString.append(testmethodHead());
-			testcaseString.append(metamorphictestBody(metamorphicTest));
-			testcaseString.append(testmethodFoot());
+			testmethods.append(metamorphictestBody(metamorphicTest));
 		}
 		
 		for( SmokeTest smokeTest : smokeTests ) {
-			testcaseString.append(testmethodHead());
-			testcaseString.append(smoketestBody(smokeTest));
-			testcaseString.append(testmethodFoot());
+			testmethods.append(smoketestBody(smokeTest));
 		}
 		
-		testcaseString.append(classFoot());
-		
-		return testcaseString.toString();
+		classBody = classBody.replaceAll("<<<PACKAGENAME>>>", getPackageName());
+		classBody = classBody.replaceAll("<<<CLASSNAME>>>", getClassName());
+		classBody = classBody.replaceAll("<<<METHODS>>>", testmethods.toString());
+		return classBody;
 	}
 	
 	/**
@@ -92,103 +87,19 @@ public class TestclassGenerator {
 	public String getClassName() {
 		return classifierUnderTest.createClassifier().getClass().getSimpleName() + "AtomlTest";
 	}
-	
-	/**
-	 * @return code for package name generation
-	 */
-	private String packageName() {
-		return "package " + getPackageName() + ";\n";
-	}
-	
-	/**
-	 * @return code for JUnit 4 imports
-	 */
-	private String junitImports() {
-		return "import static org.junit.Assert.*;\n" + 
-				"import org.junit.Test;\n";
-	}
-	
-	/**
-	 * @return codes for imports required by the generated tests
-	 */
-	private String atomlImports() {
-		return "import javax.annotation.Generated;\n" +
-			   "import java.io.BufferedReader;\n" +
-			   "import java.io.InputStreamReader;\n" +
-	           "import java.io.IOException;\n" +
-			   "import weka.classifiers.Classifier;\n" + 
-               "import weka.core.Instances;\n" + 
-               "import weka.core.Instance;\n";
-	}
-	
-	/**
-	 * @return code for the class header
-	 */
-	private String classHead() {
-		return "/**\n" +
-	           " * Automatically generated smoke and metamorphic tests.\n" + 
-               " */\n" + 
-	           "@Generated(\"atoml.testgen.TestclassGenerator\")\n" +
-				"public class " + getClassName() + " {\n";
-	}
 
-	/**
-	 * @return code for the class footer
-	 */
-	private String classFoot() {
-		return "}";
-	}
-	
-	/**
-	 * @return code for the test method head
-	 */
-	private String testmethodHead() {
-		return "\n    @Test\n";
-	}
-	
-	/**
-	 * @return code for the test method footer
-	 */
-	private String testmethodFoot() {
-		return "    }\n";
-	}
-	
 	/**
 	 * @param smokeTest the smoke test
 	 * @return body for a smoke test method
 	 */
 	private String smoketestBody(SmokeTest smokeTest) {
-		return "    public void " + smokeTest.getName() + "_SmokeTest() throws Exception {\n" +
-			   "        for(int iter=1; iter<=" + iterations + "; iter++) {\n" +
-	           "            Instances data;\n" + 
-	           "            InputStreamReader file = new InputStreamReader(\n" + 
-	           "                    this.getClass().getResourceAsStream(\"/smoketest_" + smokeTest.getName() + "_\" + iter + \"_training.arff\"));\n" +
-	           "            try(BufferedReader reader = new BufferedReader(file);) {\n" + 
-	           "                data = new Instances(reader);\n" + 
-	           "                reader.close();\n" + 
-	           "            }\n" + 
-	           "            catch (IOException e) {\n" + 
-	           "                throw new RuntimeException(\"error reading file:  smoketest_" + smokeTest.getName() + "_\" + iter + \"_training.arff\", e);\n" + 
-	           "            }\n" +
-	           "            data.setClassIndex(data.numAttributes()-1);\n" +
-	           "            Instances testdata;\n" + 
-	           "            InputStreamReader testfile = new InputStreamReader(\n" + 
-	           "                    this.getClass().getResourceAsStream(\"/smoketest_" + smokeTest.getName() + "_\" + iter + \"_test.arff\"));\n" +
-	           "            try(BufferedReader reader = new BufferedReader(testfile);) {\n" + 
-	           "                testdata = new Instances(reader);\n" + 
-	           "                reader.close();\n" + 
-	           "            }\n" + 
-	           "            catch (IOException e) {\n" + 
-	           "                throw new RuntimeException(\"error reading file:  smoketest_" + smokeTest.getName() + "_\" + iter + \"_test.arff\", e);\n" + 
-	           "            }\n" + 
-	           "            testdata.setClassIndex(testdata.numAttributes()-1);\n" +
-		       "            Classifier classifier = new " +  classifierUnderTest.createClassifier().getClass().getSimpleName() + "();\n" + 
-		       "            classifier.buildClassifier(data);\n" + 
-		       "            for (Instance instance : testdata) {\n" + 
-		       "                classifier.classifyInstance(instance);\n" + 
-		       "                classifier.distributionForInstance(instance);\n" + 
-		       "            }\n" +
-		       "        }\n";
+		@SuppressWarnings("resource")
+		String methodBody = new Scanner(this.getClass().getResourceAsStream("/junit-smoketest.template"), "UTF-8").useDelimiter("\\A").next();
+		
+		methodBody = methodBody.replaceAll("<<<NAME>>>", smokeTest.getName());
+		methodBody = methodBody.replaceAll("<<<CLASSIFIER>>>", classifierUnderTest.createClassifier().getClass().getSimpleName());
+		methodBody = methodBody.replaceAll("<<<ITERATIONS>>>", Integer.toString(iterations));
+		return methodBody;
 	}
 	
 	/**
@@ -206,46 +117,15 @@ public class TestclassGenerator {
 			throw new RuntimeException("could not generate unit tests, unknown morph test class");
 		}
 		
-		return "    public void " + metamorphicTest.getName() + "_MorphTest() throws Exception {\n" +
-		           "        for(int iter=1; iter<=" + iterations + "; iter++) {\n" +
-		           "            Instances data;\n" + 
-		           "            InputStreamReader originalFile = new InputStreamReader(\n" + 
-		           "                     this.getClass().getResourceAsStream(\"/morphtest_" + metamorphicTest.getName() + "_\" + iter + \"_original.arff\"));\n" +
-		           "            try(BufferedReader reader = new BufferedReader(originalFile);) {\n" + 
-		           "                data = new Instances(reader);\n" + 
-		           "                reader.close();\n" + 
-		           "            }\n" + 
-		           "            catch (IOException e) {\n" + 
-		           "                throw new RuntimeException(\"error reading file:  morphtest_" + metamorphicTest.getName() + "_\" + iter + \"_original.arff\", e);\n" + 
-		           "            }\n" + 
-		           "            data.setClassIndex(data.numAttributes()-1);\n" +
-		           "            Instances morphedData;\n" + 
-		           "            InputStreamReader morphedFile = new InputStreamReader(\r\n" + 
-		           "                     this.getClass().getResourceAsStream(\"/morphtest_" + metamorphicTest.getName() + "_\" + iter + \"_morphed.arff\"));\n" +
-		           "            try(BufferedReader reader = new BufferedReader(morphedFile);) {\n" + 
-		           "                morphedData = new Instances(reader);\n" + 
-		           "                reader.close();\n" + 
-		           "            }\n" + 
-		           "            catch (IOException e) {\n" + 
-		           "                throw new RuntimeException(\"error reading file:  morphtest_" + metamorphicTest.getName() + "_\" + iter + \"_morphed.arff\", e);\n" + 
-		           "            }\n" +
-		           "            morphedData.setClassIndex(morphedData.numAttributes()-1);\n" +
-		           "            Classifier classifier = new " +  classifierUnderTest.createClassifier().getClass().getSimpleName() + "();\n" + 
-		           "		    classifier.buildClassifier(data);\n" + 
-		           "            Classifier morphedClassifier = new " +  classifierUnderTest.createClassifier().getClass().getSimpleName() + "();\n" + 
-		           "            morphedClassifier.buildClassifier(morphedData);\n" + 
-		           "            int violations = 0;\n" + 
-		           "            for (int i = 0; i < data.size(); i++) {\n" + 
-		           "                double originalClass = classifier.classifyInstance(data.instance(i));\n" + 
-		           morphClass + 
-		           "                if (!(" + metamorphicTest.relationAsString() + ")) {\n" +
-		           "                    violations++;\n" + 
-		           "                }\n" + 
-		           "            }\n" + 
-		           "            if (violations > 0) {\n" + 
-		           "                assertEquals(\"metamorphic relation broken: \" + violations + \" violations\",0, violations);\n" + 
-		           "            }\n" +
-		           "        }\n";
+		@SuppressWarnings("resource")
+		String methodBody = new Scanner(this.getClass().getResourceAsStream("/junit-morphtest.template"), "UTF-8").useDelimiter("\\A").next();
+		
+		methodBody = methodBody.replaceAll("<<<NAME>>>", metamorphicTest.getName());
+		methodBody = methodBody.replaceAll("<<<CLASSIFIER>>>", classifierUnderTest.createClassifier().getClass().getSimpleName());
+		methodBody = methodBody.replaceAll("<<<ITERATIONS>>>", Integer.toString(iterations));
+		methodBody = methodBody.replaceAll("<<<MORPHCLASS>>>", morphClass);
+		methodBody = methodBody.replaceAll("<<<MORPHRELATION>>>", metamorphicTest.relationAsString());
+		return methodBody;
 	}
 	
 }
