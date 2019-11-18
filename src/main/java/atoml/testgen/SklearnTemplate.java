e0f6a80fb6d61c9cfcafaef4db289e17177cde5c
sherbold <herbold@cs.uni-goettingen.de>
2019-11-18 12:19:30 +0100

    started porting new features to sklearn: parametrized and mysql
    currently does not support usage with assertions, timeouts will
    not be reported to mysql db, algorithms without parameters produce
    invalid python (empty kwargs)

--- src/main/java/atoml/testgen/SklearnTemplate.java
+++ src/main/java/atoml/testgen/SklearnTemplate.java
@@ -2,9 +2,9 @@ package atoml.testgen;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import atoml.classifiers.Algorithm;
-import atoml.classifiers.Parameter;
 import atoml.metamorphic.MetamorphicTest;
 import atoml.smoke.SmokeTest;
 
@@ -16,17 +16,11 @@ public class SklearnTemplate implements TemplateEngine {
 	final Algorithm algorithmUnderTest;
 	
 	/**
-	 * parameter string for the algorithm
-	 */
-	final String parameterString;
-	
-	/**
 	 * Constructor. Creates a new WekaTemplate. 
 	 * @param algorithmUnderTest
 	 */
 	public SklearnTemplate(Algorithm algorithmUnderTest) {
 		this.algorithmUnderTest = algorithmUnderTest;
-		parameterString = getDefaultParameters(algorithmUnderTest);
 	}
 	
 	@Override
@@ -42,15 +36,35 @@ public class SklearnTemplate implements TemplateEngine {
 	@Override
 	public Map<String, String> getClassReplacements() {
 		String importStatement = "from " + algorithmUnderTest.getPackage() + " import " + algorithmUnderTest.getClassName();;
+		
+		StringBuilder parameterString = new StringBuilder();
+		parameterString.append("    params = [");
+		boolean isFirst = true;
+		if( algorithmUnderTest.getParameterCombinations().size()>0 ) {
+			for( Map<String,String> parameterCombination :  algorithmUnderTest.getParameterCombinations()) {
+				String curParamString = getParameterString(parameterCombination);
+				if( !isFirst ) {
+					parameterString.append("              ");
+				}
+				parameterString.append("(\"" + curParamString + "\", " + curParamString +  "),\n");
+				isFirst = false;
+			}
+			parameterString.append("             ]");
+		} else {
+			// TODO This does not work properly yet, because it would generate an empty kwargs dict
+			parameterString = parameterString.append("            \"{}\", {}]");
+		}
+		
 		Map<String, String> replacements = new HashMap<>();
 		replacements.put("<<<IMPORTCLASSIFIER>>>", importStatement);
+		replacements.put("<<<HYPERPARAMETERS>>>", parameterString.toString());
 		return replacements;
 	}
 
 	@Override
 	public Map<String, String> getSmoketestReplacements(SmokeTest smokeTest) {
 		Map<String, String> replacements = new HashMap<>();
-		replacements.put("<<<CLASSIFIER>>>", algorithmUnderTest.getClassName()+parameterString);
+		replacements.put("<<<CLASSIFIER>>>", algorithmUnderTest.getClassName()+"(**kwargs)");
 		return replacements;
 	}
 
@@ -74,39 +88,38 @@ public class SklearnTemplate implements TemplateEngine {
 		String morphRelation;
 		switch(metamorphicTest.getPredictionRelation()) {
 		case EQUAL:
-			morphRelation = "self.assertTrue((prediction_original==prediction_morph).all())";
+			morphRelation = "prediction_expected = prediction_original;\n";
+			morphRelation += "            scores_expected = scores_original[:,0]";
 			break;
 		case INVERTED:
-			morphRelation = "self.assertFalse((prediction_original==prediction_morph).any())";
+			morphRelation = "prediction_expected = abs(1-prediction_original)\n";
+			morphRelation += "            scores_expected = scores_original[:,1]";
 			break;
 		default:
 			throw new RuntimeException("could not generate tests, unknown morph prediction relation type");
 		}
 		
 		Map<String, String> replacements = new HashMap<>();
-		replacements.put("<<<CLASSIFIER>>>", algorithmUnderTest.getClassName()+parameterString);
+		replacements.put("<<<CLASSIFIER>>>", algorithmUnderTest.getClassName()+"(**kwargs)");
 		replacements.put("<<<MORPHTESTDATA>>>", morphTestdata);
 		replacements.put("<<<MORPHCLASSINDEX>>>", morphClassIndex);
-		replacements.put("<<<MORPHRELATION>>>", morphRelation);
+		replacements.put("<<<EXPECTEDMORPHEDCLASS>>>", morphRelation);
 		return replacements;
 	}
 	
 	/**
-	 * creates a string that describes the default parameters
+	 * creates a string for the test of a parameter combination
 	 * @return parameters string
 	 */
-	private static String getDefaultParameters(Algorithm algorithm) {
+	private static String getParameterString(Map<String, String> parameterCombination) {
 		StringBuilder parameters = new StringBuilder();
-		parameters.append("(");
-		for( Parameter parameter : algorithm.getParameters()) {
-			parameters.append(parameter.getName()+"=");
-			parameters.append(parameter.getDefaultValue()+",");
-			
+		parameters.append("{");
+		for( Entry<String, String> parameter : parameterCombination.entrySet()) {
+			parameters.append("'"+parameter.getKey()+"':");
+			// TODO need to use parameter type here to decide whether there are quotes
+			parameters.append(""+parameter.getValue()+",");
 		}
-		if( algorithm.getParameters().size()>0 ) {
-			parameters.replace(parameters.length()-1, parameters.length(), "");
-		}
-		parameters.append(")");
+		parameters.append("}");
 		return parameters.toString();
 	}
 }
