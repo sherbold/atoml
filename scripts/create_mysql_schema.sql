c88d0590f3aade8808a32d14ede6c6f9591490fe
Earth50 <haar.tobias@gmail.com>
2020-06-24 12:32:43 +0200

    mysql schema update

--- scripts/create_mysql_schema.sql
+++ scripts/create_mysql_schema.sql
@@ -15,6 +15,10 @@ CREATE TABLE `results_morphtests` (
   `passed_exact_class` tinyint(1) DEFAULT NULL,
   `passed_stat_class` tinyint(1) DEFAULT NULL,
   `passed_stat_score` tinyint(1) DEFAULT NULL,
+  `passed` tinyint(1) DEFAULT NULL,
+  `exception` varchar(200) DEFAULT NULL,
+  `message` varchar(1000) DEFAULT NULL,
+  `stacktrace` longtext DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=26590 DEFAULT CHARSET=latin1;
 
@@ -47,7 +51,11 @@ CREATE VIEW `morphtest_view` AS select
   `results_morphtests`.`passed_exact_score` AS `passed_exact_score`,
   `results_morphtests`.`passed_exact_class` AS `passed_exact_class`,
   `results_morphtests`.`passed_stat_class` AS `passed_stat_class`,
-  `results_morphtests`.`passed_stat_score` AS `passed_stat_score`
+  `results_morphtests`.`passed_stat_score` AS `passed_stat_score`,
+  `results_morphtests`.`passed` AS `passed`,
+  `results_morphtests`.`exception` AS `exception`,
+  `results_morphtests`.`message` AS `message`,
+  `results_morphtests`.`stacktrace` AS `stacktrace`
 from `results_morphtests`;
 
 CREATE VIEW `smoketest_view` AS select
