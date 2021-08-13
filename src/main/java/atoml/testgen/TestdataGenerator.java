01545d2cc0fb4de4eb84c5422adea9c9b2a25870
sherbold <herbold@cs.uni-goettingen.de>
2021-08-13 12:34:10 +0200

    fix precision problem with generated data

--- src/main/java/atoml/testgen/TestdataGenerator.java
+++ src/main/java/atoml/testgen/TestdataGenerator.java
@@ -1,10 +1,6 @@
 package atoml.testgen;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,6 +18,7 @@ import atoml.data.DataGenerator;
 import atoml.metamorphic.MetamorphicTest;
 import atoml.smoke.SmokeTest;
 import weka.core.Instances;
+import weka.core.converters.ArffSaver;
 
 /**
  * Generates the data for unit tests
@@ -33,27 +30,27 @@ public class TestdataGenerator {
 	 * smoke tests for which data is generated
 	 */
 	private List<SmokeTest> smokeTests;
-	
+
 	/**
 	 * metamorphic tests for which data is generated
 	 */
 	private List<MetamorphicTest> metamorphicTests;
-	
+
 	/**
 	 * number of features for generated data
 	 */
 	final int numFeatures;
-	
+
 	/**
 	 * number of instances for generated data
 	 */
 	final int numInstances;
-	
+
 	/**
 	 * number of iterations for the test data generation
 	 */
 	private final int iterations;
-	
+
 	/**
 	 * creates a new TestdataGenerator
 	 * @param smokeTests smoke tests for which data is generated
@@ -69,14 +66,14 @@ public class TestdataGenerator {
 		this.numInstances = numInstances;
 		this.iterations = iterations;
 	}
-	
+
 	/**
 	 * generates the test data
 	 * @param datapath path where the generated data is stored
 	 * @return name of the datasets on which morph tests are based
 	 */
 	public List<DataDescription> generateTestdata(String datapath) {
-		
+
 		Path smokePath = Paths.get(datapath).resolve("smokedata");
 		Path morphPath = Paths.get(datapath).resolve("morphdata");
 
@@ -86,8 +83,8 @@ public class TestdataGenerator {
 		} catch (IOException e) {
 			throw new RuntimeException("could not create folder for test data", e);
 		}
-		
-		
+
+
 		int[] featureTypes = IntStream.generate(() -> 10).limit(numFeatures).toArray();
 		List<DataDescription> morphtestDataDescriptions = new ArrayList<>();
 		morphtestDataDescriptions.add(new DataDescription("RANDNUM", numFeatures, 0, numInstances, new UniformRealDistribution(), 0.5, null, Arrays.asList(FeatureType.UNIT)));
@@ -100,24 +97,16 @@ public class TestdataGenerator {
 		morphtestDataDescriptions.add(new DataDescription("UNBALANCE", "/morphdata/unbalanced.arff", true, false, Arrays.asList(FeatureType.FLOAT)));
 		morphtestDataDescriptions.add(new DataDescription("WEATHERNOMINAL", "/morphdata/weathernominal.arff", false, true, Arrays.asList(FeatureType.CATEGORICAL)));
 		morphtestDataDescriptions.add(new DataDescription("WEATHERNUMERIC", "/morphdata/weathernumeric.arff", true, true, Arrays.asList(FeatureType.CATEGORICAL, FeatureType.POSITIVEFLOAT)));
-		
+
 		for( int iteration=1; iteration<=this.iterations; iteration++) {
 			for( SmokeTest smokeTest : smokeTests ) {
 				if( smokeTest.isRandomized() || iteration==1 ) {
 					smokeTest.generateData(numFeatures, numInstances, iteration);
-					try(BufferedWriter writer = new BufferedWriter(new FileWriter(datapath + "smokedata/" + smokeTest.getName() + "_" + iteration + "_training.arff"))) {
-						writer.write(smokeTest.getData().toString());
-					} catch(IOException e) {
-						throw new RuntimeException("could not write data for smoke test " + smokeTest.getName(), e);
-					}
-					try(BufferedWriter writer = new BufferedWriter(new FileWriter(datapath + "smokedata/" + smokeTest.getName() + "_" + iteration + "_test.arff"))) {
-						writer.write(smokeTest.getTestData().toString());
-					} catch(IOException e) {
-						throw new RuntimeException("could not write data for smoke test " + smokeTest.getName(), e);
-					}
+					writeArffFromInstances(datapath + "smokedata/" + smokeTest.getName() + "_" + iteration + "_training.arff", smokeTest.getData());
+					writeArffFromInstances(datapath + "smokedata/" + smokeTest.getName() + "_" + iteration + "_test.arff", smokeTest.getTestData());
 				}
 			}
-			
+
 			for( DataDescription dataDescription : morphtestDataDescriptions ) {
 				if( dataDescription.isRandomized() || iteration==1 ) {
 					Instances originalData = null;
@@ -131,21 +120,13 @@ public class TestdataGenerator {
 					default:
 						throw new RuntimeException("unsupported data description type: " + dataDescription.getType());
 					}
-					
-					try (BufferedWriter writer = new BufferedWriter(new FileWriter(datapath + "morphdata/" + dataDescription + "_" + iteration + ".arff"))) {
-						writer.write(originalData.toString());
-					} catch(Exception e) {
-						throw new RuntimeException("could not write data " + dataDescription.getName(), e);
-					}
+
+					writeArffFromInstances(datapath + "morphdata/" + dataDescription + "_" + iteration + ".arff", originalData);
 					for(MetamorphicTest metamorphicTest : metamorphicTests) {
 						metamorphicTest.setSeed(iteration);
 						if( metamorphicTest.isCompatibleWithData(dataDescription) ) {
 							Instances morphedData = metamorphicTest.morphData(originalData);
-							try (BufferedWriter writer = new BufferedWriter(new FileWriter(datapath + "morphdata/" + dataDescription + "_" + iteration + "_" + metamorphicTest.getName() + ".arff"))) {
-								writer.write(morphedData.toString());
-							} catch(Exception e) {
-								throw new RuntimeException("could not write data for metamorphic test " + metamorphicTest.getName(), e);
-							}
+							writeArffFromInstances(datapath + "morphdata/" + dataDescription + "_" + iteration + "_" + metamorphicTest.getName() + ".arff", morphedData);
 						}
 					}
 				}
@@ -153,7 +134,38 @@ public class TestdataGenerator {
 		}
 		return morphtestDataDescriptions;
 	}
-	
+
+	private void writeArffFromInstances(String path, Instances data) {
+		ArffSaver writer = new ArffSaver();
+		writer.setInstances(data);
+		writer.setMaxDecimalPlaces(20);
+		try {
+			writer.setFile(new File(path));
+			writer.writeBatch();
+		} catch (IOException e) {
+			throw new RuntimeException("could not write data " + path, e);
+		}
+
+		/**
+		 * Code for debugging if data is written as expected, uncomment if required for testing
+		 */
+		/*
+		Instances loadedData;
+		try(BufferedReader reader = new BufferedReader(new FileReader(path))) {
+			loadedData = new Instances(reader);
+		}
+		catch (IOException e) {
+			throw new RuntimeException("error loading ARFF after writing for validation: " + path, e);
+		}
+		for( int i=0; i<10; i++) {
+			int j=0;
+			if( data.instance(i).value(j) != loadedData.instance(i).value(j)) {
+				System.out.println(path + ": " + data.instance(i).value(j) + " - " + loadedData.instance(i).value(j));
+			}
+		}
+		*/
+	}
+
 	private Instances readArffFromResource(String resource) {
 		Instances data;
         InputStreamReader originalFile = new InputStreamReader(
